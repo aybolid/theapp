8623eeec110bca9be91b065d61a208909876c110
matviy <matviy13@proton.me>
2026-02-18 20:11:00 +0200

    signin

--- apps/webapp/src/lib/query/auth.ts
+++ apps/webapp/src/lib/query/auth.ts
@@ -1,6 +1,11 @@
 import type { Treaty } from "@elysiajs/eden";
 import { type UseMutationOptions, useMutation } from "@tanstack/react-query";
-import type { SignupBody, SignupCreated } from "@theapp/server/schemas";
+import type {
+  SigninBody,
+  SigninOk,
+  SignupBody,
+  SignupCreated,
+} from "@theapp/server/schemas";
 import { server } from "../api";
 
 export function useSignupMutation(
@@ -17,6 +22,30 @@ export function useSignupMutation(
     mutationKey: ["signup"],
     mutationFn: async (data: SignupBody) => {
       const resp = await server.api.auth.signup.post(data);
+      if (resp.error) {
+        throw resp.error;
+      } else {
+        return resp.data;
+      }
+    },
+    ...options,
+  });
+}
+
+export function useSigninMutation(
+  options?: Omit<
+    UseMutationOptions<
+      SigninOk,
+      Treaty.Error<typeof server.api.auth.signin.post>,
+      SigninBody
+    >,
+    "mutationKey" | "mutationFn"
+  >,
+) {
+  return useMutation({
+    mutationKey: ["signin"],
+    mutationFn: async (data: SigninBody) => {
+      const resp = await server.api.auth.signin.post(data);
       if (resp.error) {
         throw resp.error;
       } else {
--- apps/webapp/src/routes/_no_auth/signin.tsx
+++ apps/webapp/src/routes/_no_auth/signin.tsx
@@ -1,9 +1,173 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "@tanstack/react-form";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { signinBodySchema } from "@theapp/server/schemas";
+import { Button } from "@theapp/ui/components/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@theapp/ui/components/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@theapp/ui/components/field";
+import { Input } from "@theapp/ui/components/input";
+import { Spinner } from "@theapp/ui/components/spinner";
+import { extractZodIssuesFromValidationError } from "@theapp/webapp/lib/api";
+import { setZodIssuesAsFieldErrors } from "@theapp/webapp/lib/forms";
+import { useSigninMutation } from "@theapp/webapp/lib/query/auth";
 
 export const Route = createFileRoute("/_no_auth/signin")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_no_auth/signin"!</div>;
+  const signinMutation = useSigninMutation({
+    onError: (err) => {
+      const issues =
+        err.status === 422
+          ? extractZodIssuesFromValidationError(err.value)
+          : null;
+
+      switch (err.status) {
+        case 400:
+          form.setErrorMap({
+            onSubmit: { form: { message: err.value }, fields: {} },
+          });
+          break;
+        // biome-ignore lint/suspicious/noFallthroughSwitchClause: expected fallthrough
+        case 422:
+          if (issues) {
+            setZodIssuesAsFieldErrors(form, issues);
+            break;
+          }
+        default:
+          form.setErrorMap({
+            onSubmit: {
+              form: { message: "An unknown error occurred" },
+              fields: {},
+            },
+          });
+      }
+    },
+  });
+
+  const form = useForm({
+    formId: "signin-form",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onSubmit: signinBodySchema,
+    },
+    onSubmit: ({ value }) => {
+      signinMutation.mutate(value);
+    },
+  });
+
+  const isBusy = signinMutation.isPending || form.state.isSubmitting;
+
+  return (
+    <main className="grid h-screen place-items-center">
+      <Card className="w-full max-w-md">
+        <CardHeader className="border-b">
+          <CardTitle>Sign in</CardTitle>
+          <CardDescription>
+            Enter your details below to access your account.
+          </CardDescription>
+        </CardHeader>
+        <form
+          className="contents"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <CardContent className="space-y-4">
+            <FieldGroup>
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>
+                        Email address
+                      </FieldLabel>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        aria-invalid={isInvalid}
+                        type="email"
+                        placeholder="email@example.com"
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+            </FieldGroup>
+            <FieldGroup>
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        aria-invalid={isInvalid}
+                        type="password"
+                        placeholder="••••••••"
+                      />
+                      {isInvalid && (
+                        <FieldError errors={field.state.meta.errors} />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+            </FieldGroup>
+            {!form.state.isFormValid && (
+              <FieldGroup>
+                <FieldError errors={form.state.errors} />
+              </FieldGroup>
+            )}
+            <FieldGroup>
+              <Button disabled={isBusy}>
+                {isBusy && <Spinner />}
+                <span>Sign in</span>
+              </Button>
+            </FieldGroup>
+          </CardContent>
+        </form>
+        <CardFooter>
+          <p className="text-muted-foreground text-sm">
+            Do not have an account?{" "}
+            <Link className="text-foreground hover:underline" to="/signup">
+              Create account
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
